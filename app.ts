0e9ec46ebef91035dba6cca5ec642bcec3482c66
Jason McCann <1457388+alias-zon3@users.noreply.github.com>
2021-07-24 17:02:25 +0100

    Adding express and basic application
    Adding express package as a dependency
    Creating a basic app.ts which will serve as an initial placeholder node app

---
+++ app.ts
@@ -0,0 +1,11 @@
+//Import required namespaces
+import express from 'express';
+
+//Construct our single express instance and define the port we want to use
+const app = express();
+const port = 3000;
+
+//Start a basic application which does nothing but log for the time being
+app.listen(port, () => {
+    console.log(`Currently listening on port ${port}`)
+});
